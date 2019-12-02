75207547eebc4ab221ef92fad0175a25cddcf060
Miles <milotheengineer@gmail.com>
2019-12-02 15:25:23 +0000

    Managed to complete the Products side of the WPF application. The customers menu is the only thing left i need to fix

--- AdventureWorks.DataLayer/ProductManager.vb
+++ AdventureWorks.DataLayer/ProductManager.vb
@@ -1,3 +1,46 @@
-﻿Public Class Class1
+﻿Imports System.Collections.ObjectModel
+Imports System.Configuration
+Imports AdventureWorks.EntityLayer
+Imports Common.Library
+
+
+Public Class ProductManager
+    Inherits DataManagerBase
+
+    Function LoadProducts() As ObservableCollection(Of Product)
+        Return LoadProducts(Nothing)
+    End Function
+
+    Function LoadProducts(ByVal startingFilePath As String) As ObservableCollection(Of Product)
+        ' Create a new instance of products
+        Dim ret = New ObservableCollection(Of Product)()
+
+        Try
+            ' Attempt to read from XML file
+            Dim doc = MyBase.LoadXElement(
+                ConfigurationManager.AppSettings("ProductsFile"),
+                startingFilePath)
+
+            Dim products = From prod In doc.<Product>
+                           Select New Product With {
+                               .ProductID = Convert.ToInt32(prod.Element("ProductID").Value),
+                               .Name = prod.Element("Name").Value,
+                               .ProductNumber = prod.Element("ProductNumber").Value,
+                               .Color = prod.Element("Colour").Value,
+                               .Weight = Convert.ToDecimal(prod.Element("Weight").Value),
+                               .StandardCost = Convert.ToDecimal(prod.Element("StandardCost").Value),
+                               .SellStartDate = Convert.ToDateTime(prod.Element("SellStartDate").Value),
+                               .SellEndDate = Convert.ToDateTime(prod.Element("SellEndDate").Value)
+                           }
+
+
+            ret = New ObservableCollection(Of Product)(products.ToList)
+        Catch ex As Exception
+            System.Diagnostics.Debug.WriteLine(ex.ToString())
+
+        End Try
+
+        Return ret
+    End Function
 
 End Class
--- AdventureWorks.ViewModelLayer/ProductViewModel1.vb
+++ AdventureWorks.ViewModelLayer/ProductViewModel1.vb
@@ -1,33 +1,30 @@
-﻿Imports AdventureWorks.EntityLayer
+﻿Imports System.Collections.ObjectModel
+Imports AdventureWorks.DataLayer
+Imports AdventureWorks.EntityLayer
 Imports Common.Library
 
 Public Class ProductViewModel1
     Inherits ViewModelBase
 
     Sub New()
-        LoadProduct(680)
+        LoadProducts()
     End Sub
 
+    Public Property Products As ObservableCollection(Of Product)
     Public Property Entity As Product
 
-    Function LoadProduct(ByVal productID As Integer) As Product
-        Return LoadProduct(productID, Nothing)
+    Function LoadProducts() As ObservableCollection(Of Product)
+        Return LoadProducts(Nothing)
     End Function
 
-    Function LoadProduct(ByVal produtID As Integer, ByVal startingFilePath As String) As Product
 
-        ' Hard code an entity
-        Entity = New Product() With {
-            .ProductID = 680,
-            .Name = "l Rad Frame - black, 58",
-            .ProductNumber = "FR-R92B-58",
-            .Color = "Black",
-            .Size = "58",
-            .Weight = 1016.04D,
-            .StandardCost = 1059.31D
-        }
+    Function LoadProducts(ByVal startingFilePath As String) As ObservableCollection(Of Product)
+        ' Create a new instnace of the product manager class
+        Dim mgr = New ProductManager
 
-        Return Entity
+        Products = mgr.LoadProducts(startingFilePath)
+
+        Return Products
     End Function
 
 End Class
